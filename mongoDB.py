49981e26fc452b57c5f29b3ba9832dc9bab9f9eb
Timo <timo.schessl@gmx.de>
2020-05-03 18:07:24 +0200

    Look at database and inserting new data works.

--- mongoDB.py
+++ mongoDB.py
@@ -1,6 +1,162 @@
 import pymongo
 from pymongo import MongoClient
 import tkinter as tk
+import tkinter.messagebox
+
+
+def insertIntoDatabase(firstName, lastName, age, job, description):
+   try:
+    collection.insert_one({"First name":firstName,"Last name":lastName,"Age":age,"Job":job,"Description":description})
+    tkinter.messagebox.showinfo("Message","Inserted data successfully.")
+   except:
+    tkinter.messagebox.showinfo("Error")
+
+
+def deleteFromDatabase(id):
+ try:
+  collection.delete_one({"_id":id})
+  tkinter.messagebox.showinfo("Message", "Deleted data successfully.")
+ except:
+  tkinter.messagebox.showinfo("Error")
+
+
+def initStartGUI():
+ window = tk.Tk()
+ window.title("Database GUI")
+ window.resizable(False, False)
+ window.geometry("400x300")
+ window.eval('tk::PlaceWindow . center')
+
+ buttonPost = tk.Button(window,text="Insert data",command=lambda:initInsertGUI())
+ buttonPost.pack()
+
+ buttonDelete = tk.Button(window,text="Delete data",command=lambda:initDeleteGUI())
+ buttonDelete.pack()
+
+ buttonUpdate = tk.Button(window,text="Update data")
+ buttonUpdate.pack()
+
+ buttonGet = tk.Button(window,text="Get data",command=lambda:initGetGUI())
+ buttonGet.pack()
+
+ buttonShow = tk.Button(window,text="Look at database",command=lambda :showDatabaseGUI())
+ buttonShow.pack()
+
+ window.mainloop()
+
+def getFromDatabase(id):
+   return str(collection.find_one({"_id":""+id}))
+
+
+def initGetGUI():
+ window = tk.Tk()
+ window.title("Database GUI")
+ window.resizable(False, False)
+ window.geometry("400x300")
+ window.eval('tk::PlaceWindow . center')
+
+ label1 = tk.Label(window, text="Enter the id of the object:")
+ label1.pack()
+
+ textField1 = tk.Entry(window, width="50")
+ textField1.pack()
+
+ textarea = tk.Text(window, width="30", height="10")
+ textarea.pack()
+
+ buttonGet = tk.Button(window, text="Get data",
+                          command=lambda: textarea.insert(tk.END,getFromDatabase(textField1.get())))
+ buttonGet.pack()
+
+ window.mainloop()
+
+
+def initDeleteGUI():
+ window = tk.Tk()
+ window.title("Database GUI")
+ window.resizable(False, False)
+ window.geometry("400x300")
+ window.eval('tk::PlaceWindow . center')
+
+ label1 = tk.Label(window, text="Enter the id of the object:")
+ label1.pack()
+
+ textField1 = tk.Entry(window, width="50")
+ textField1.pack()
+
+ buttonDelete = tk.Button(window, text="Delete",
+                       command=lambda: deleteFromDatabase(textField1.get()))
+ buttonDelete.pack()
+
+ window.mainloop()
+
+
+
+def initInsertGUI():
+ window = tk.Tk()
+ window.title("Database GUI")
+ window.resizable(False, False)
+ window.geometry("400x300")
+ window.eval('tk::PlaceWindow . center')
+
+ label1 = tk.Label(window, text="First name:")
+ label1.pack()
+
+ textField1 = tk.Entry(window)
+ textField1.pack()
+
+ label2 = tk.Label(window, text="Last name:")
+ label2.pack()
+
+ textField2 = tk.Entry(window)
+ textField2.pack()
+
+ label3 = tk.Label(window, text="Age:")
+ label3.pack()
+
+ textField3 = tk.Entry(window)
+ textField3.pack()
+
+ label4= tk.Label(window, text="Job:")
+ label4.pack()
+
+ textField4 = tk.Entry(window)
+ textField4.pack()
+
+ label5 = tk.Label(window, text="Description:")
+ label5.pack()
+
+ textField5 = tk.Entry(window)
+ textField5.pack()
+
+ buttonGet = tk.Button(window, text="Insert into database",
+                       command=lambda :insertIntoDatabase(textField1.get(),textField2.get(),textField3.get(),textField4.get(),textField5.get()))
+ buttonGet.pack()
+
+ window.mainloop()
+
+
+def getAllData():
+    return list(collection.find())
+
+
+
+
+def showDatabaseGUI():
+ window = tk.Tk()
+ window.title("Database GUI")
+ window.resizable(False, False)
+ window.geometry("400x300")
+ window.eval('tk::PlaceWindow . center')
+
+ textarea = tk.Text(window,width="30",height="10")
+ textarea.pack()
+
+ buttonShow = tk.Button(window, text="Show data", command=lambda: textarea.insert(tk.END, getAllData()))
+ buttonShow.pack()
+
+ window.mainloop()
+
 
 
 password = input("Please enter the password for mainuser:")
@@ -8,34 +164,14 @@ cluster = MongoClient("mongodb://mainuser:"+password+"@cluster-shard-00-00-0gtou
 database = cluster["test"]
 collection = database["posts"]
 try:
- collection.insert_one({"_id": 0, "Test": "Connection Test"})
+ collection.insert_one({"_id":0})
  collection.delete_one({"_id":0})
  print("Connection with database successful.")
+ initStartGUI()
 except:
  print("Wrong password.")
 
 
 
-def initStartGUI():
- window = tk.Tk()
- window.title("Enter password")
- window.resizable(False, False)
- window.geometry("400x300")
- window.eval('tk::PlaceWindow . center')
-
- label1 = tk.Label(window, text="Please enter the database password:")
- label1.pack()
-
- textinput = tk.Entry(window, width="50")
- textinput.pack()
-
- button = tk.Button(text="Enter")
- button.pack()
-
- window.mainloop()
-
-
-def insertIntoDatabase():
-    collection.insert_one({"_id":0,"firstName": "Timo2"})
 
 
